e22b52168eaa57703750c432e23a5639a6231d1d
Arthur Cavalcante <linsarthur02@gmail.com>
2024-12-17 18:14:35 -0300

    Exercicio 01 - Alterando o texto ao clicar no botão manipulando o DOM

--- execicioDOM/exercicio01/script.js
+++ execicioDOM/exercicio01/script.js
@@ -1,9 +1,8 @@
-let texto = document.querySelector("#alterar-texto");
-let textoAlterado = document.createTextNode("Texto trocado");
-let paiTexto = texto.parentNode;
 
-function clicar() {
-  onclick(() => {
-    paiTexto.replaceChild(texto, textoAlterado);
-  });
-}
+
+let btn = document.querySelector("#alterar-texto")
+let p = document.querySelector("#texto")
+
+btn.addEventListener("click", () => {
+  p.textContent = "Texto alterado pelo JavaScript"
+});
